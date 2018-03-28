d24ca811f04cc773fa52a9207a5febd369067b91
Mreenav Deka <mreenav@gmail.com>
2018-03-28 17:03:35 +0530

    Add files via upload

---
+++ download_icici_holdings.py
@@ -0,0 +1,85 @@
+'''Data missing from january 2013 and earlier '''
+from selenium import webdriver
+import cfscrape
+import time
+import zipfile
+import os
+from shutil import rmtree
+import pandas as pd
+
+def rename(date, file_path):
+	#remove file type if not required in combined workbook
+	file_types = ['Arbitrage', 'CPOF', 'Debt', 'Equity', 'FMP', 'FOF', 'Gold', 'Hybrid', 'Interval', 'MYF']
+	excel_files = [] 
+	df_list = {} #dictionary to store excel sheets as dataframes referenced by sheet names
+	file_names = [] #file paths, to be deleted later
+	for file in os.listdir(file_path):
+		if (".xls" in file) and not (file.startswith('icici')):
+			for file_type in file_types:
+				if file_type in file:
+					excel_files.append(pd.ExcelFile(os.path.join(file_path,file)))
+					file_names.append(os.path.join(file_path,file))
+	for file in excel_files:
+		for sheet in file.sheet_names:
+			df_list[sheet] = file.parse(sheet)
+
+
+	directories = next(os.walk(file_path))[1]
+	for directory in directories:
+		for file in os.listdir(os.path.join(file_path,directory)):
+			if (".xls" in file):
+				for file_type in file_types:
+					if file_type in file:
+						excel_files.append(pd.ExcelFile(os.path.join(file_path,directory,file)))	
+		for file in excel_files:
+			for sheet in file.sheet_names:
+				df_list[sheet] = file.parse(sheet)
+					
+		rmtree(os.path.join(file_path, directory))	#delete directory
+
+	writer = pd.ExcelWriter(os.path.join(file_path, 'icici_portfolios_' + date.strftime('%Y%m') + '.xls'))
+	for sheet, data in df_list.items():
+		data.to_excel(writer, sheet)
+	writer.save()
+	
+	for file in file_names:
+		os.remove(file)			
+
+
+def download(dates):
+	file_path = 'F:\\Projects\\internship\\test\\'
+	chrome_driver = 'F:\\Projects\\internship\\birla_data\\chromedriver.exe'
+	scraper = cfscrape.create_scraper()
+	driver = webdriver.Chrome(executable_path = chrome_driver)
+	driver.get('https://www.icicipruamc.com/Downloads/MonthlyPortfolioDisclosure.aspx')
+	
+	for d in dates:
+		year = d.strftime("%y")
+		month = d.strftime("%b")
+		
+		file = driver.find_elements_by_xpath('.//a[contains(text(), "' + year + '") and contains(text(), "' + month + '") and contains(text(), "Monthly Portfolio")]')
+
+
+		if file:
+			file_link = file[0].get_attribute("href")
+			cfurl = scraper.get(file_link).content
+			save_file_name = "icici_portfolios_" + d.strftime('%Y%m') + '.zip'
+
+			if cfurl != b'':
+				print('Downloading file for ' + d.strftime('%b%Y'))
+				with open(file_path+save_file_name, 'wb') as f:
+					f.write(cfurl)
+
+			try:
+				current_file = zipfile.ZipFile(file_path + save_file_name)
+				current_file.extractall(file_path)
+				current_file.close()
+				os.remove(file_path + save_file_name)
+				rename(d, file_path)
+			except:
+				print("Could not unzip " + save_file_name)		
+
+
+	driver.close()		
+			
+			
